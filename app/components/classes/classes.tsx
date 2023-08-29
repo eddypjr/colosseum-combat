aaa279fedb5a1bab0ae06df738eb5cff9c0310b1
Edward Park <edwardpark123@gmail.com>
2023-08-29 13:06:43 -0700

    CTA, Classes, and images. will convert to webp later

---
+++ app/components/classes/classes.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+
+export default function Classes() {
+  return (
+    <section id="featured" className="page-padding py-[12.5rem] bg-white">
+      <div className="container">
+        {/* title */}
+        <div className="flex flex-col text-center items-center relative ">
+          <p className="font-semibold text-white text-[15px] relative uppercase z-10 ">
+            OUR FEATURED CLASS
+          </p>
+          {/* <img
+            src={TitleRed}
+            alt="text_bg"
+            className="w-[24rem] absolute -top-[10px]"
+          /> */}
+          <h2 className="text-[3.6rem] text-black font-bold my-10">
+            We Are Offering Best Flexible Classes
+          </h2>
+        </div>
+        <div className="grid grid-cols-4 grid-rows-2 gap-7 h-full mt-7 md1000:flex md1000:flex-col">
+          <div className="item-0-div relative">
+            <div className="item-0 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">Brazilian Jiu Jitsu</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Wednesday: 9:00am-10:00am
+              </p>
+            </div>
+          </div>
+
+          <div className="item-1-div relative">
+            <div className="item-1 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">MMA</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Friday: 10:00am-11:00am
+              </p>
+            </div>
+          </div>
+
+          <div className="item-2-div relative">
+            <div className="item-2 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">Boxing</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Saturday: 9:00am-10:00am
+              </p>
+            </div>
+          </div>
+
+          <div className="item-3-div relative">
+            <div className="item-3 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">No Gi Grappling</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Friday: 1:00pm-2:00pm
+              </p>
+            </div>
+          </div>
+
+          <div className="item-4-div relative">
+            <div className="item-4 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">Kickboxing</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Sunday: 6:00pm-7:00pm
+              </p>
+            </div>
+          </div>
+
+          <div className="item-5-div relative">
+            <div className="item-5 ease-in duration-[0.4s] p-6"></div>
+            <div className="absolute z-10 bottom-10 left-10">
+              <p className="text-white text-[3rem] font-bold">Strength & Conditioning</p>
+              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+                Monday: 4:00pm-5:00pm
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
