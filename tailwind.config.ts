e2cb820cd4b4d4b42285b27f7d7b010c7a0c9e09
Edward Park <edwardpark123@gmail.com>
2023-09-01 15:30:35 -0700

    installed fontawesome and added custom max width in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,13 @@ const config: Config = {
         primary: '#7856ff',
       },
     },
+    screens: {
+      maxXs: { max: '480px' },
+      maxSm: { max: '640px' },
+      maxMd: { max: '768px' },
+      maxLg: { max: '1024px' },
+      maxXl: { max: '1280px' },
+    },
   },
   plugins: [],
 };
