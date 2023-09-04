564129b9193d265596d7e4ce3b3af9c3ed7cd7b5
Edward Park <edwardpark123@gmail.com>
2023-09-04 15:48:44 -0700

    Classes component

--- app/components/classes/classes.tsx
+++ app/components/classes/classes.tsx
@@ -1,83 +1,92 @@
-import React from 'react';
+import Image from 'next/image';
+import TitleBg from '../../images/title-bg.svg';
+import Link from 'next/link';
 
 export default function Classes() {
   return (
-    <section id="featured" className="page-padding py-[12.5rem] bg-white">
-      <div className="container">
-        {/* title */}
-        <div className="flex flex-col text-center items-center relative ">
-          <p className="font-semibold text-white text-[15px] relative uppercase z-10 ">
-            OUR FEATURED CLASS
-          </p>
-          {/* <img
-            src={TitleRed}
-            alt="text_bg"
-            className="w-[24rem] absolute -top-[10px]"
-          /> */}
-          <h2 className="text-[3.6rem] text-black font-bold my-10">
-            We Are Offering Best Flexible Classes
-          </h2>
-        </div>
-        <div className="grid grid-cols-4 grid-rows-2 gap-7 h-full mt-7 md1000:flex md1000:flex-col">
-          <div className="item-0-div relative">
-            <div className="item-0 ease-in duration-[0.4s] p-6"></div>
+    <section id="featured" className="classes-section">
+      <div
+        className="container page-padding flex flex-col items-center relative gap-[0rem] maxXl:flex-wrap py-[12rem]"
+        style={{ margin: '0 auto' }}
+      >
+        <p className="text-white relative z-10 text-[16px] uppercase font-bold mb-10">
+          Classes
+        </p>
+        <Image
+          src={TitleBg}
+          alt="stroke"
+          className="color-logo w-[21rem] absolute -top-[-8px]"
+        />
+        <h2 className="text-[34px] text-black font-bold my-10">
+          Unleash Your Potential with Our Versatile Class Offerings
+        </h2>
+
+        <div className="grid grid-cols-4 grid-rows-2 gap-5 h-full mt-7 maxMd:flex maxMd:flex-col">
+          <Link href={'/schedule/monday'} className="item-0-div relative">
+            <div className="item-0 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
-              <p className="text-white text-[3rem] font-bold">Brazilian Jiu Jitsu</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-white text-[3rem] font-bold">
+                Brazilian Jiu Jitsu
+              </p>
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Wednesday: 9:00am-10:00am
               </p>
             </div>
-          </div>
+          </Link>
 
-          <div className="item-1-div relative">
-            <div className="item-1 ease-in duration-[0.4s] p-6"></div>
+          <Link href={'/schedule/monday'} className="item-1-div relative">
+            <div className="item-1 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">MMA</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Friday: 10:00am-11:00am
               </p>
             </div>
-          </div>
+          </Link>
 
-          <div className="item-2-div relative">
-            <div className="item-2 ease-in duration-[0.4s] p-6"></div>
+          <Link href={'/schedule/monday'} className="item-2-div relative">
+            <div className="item-2 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">Boxing</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Saturday: 9:00am-10:00am
               </p>
             </div>
-          </div>
+          </Link>
 
-          <div className="item-3-div relative">
-            <div className="item-3 ease-in duration-[0.4s] p-6"></div>
+          <Link href={'/schedule/monday'} className="item-3-div relative">
+            <div className="item-3 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
-              <p className="text-white text-[3rem] font-bold">No Gi Grappling</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-white text-[3rem] font-bold">
+                No Gi Grappling
+              </p>
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Friday: 1:00pm-2:00pm
               </p>
             </div>
-          </div>
+          </Link>
 
-          <div className="item-4-div relative">
-            <div className="item-4 ease-in duration-[0.4s] p-6"></div>
+          <Link href={'/schedule/monday'} className="item-4-div relative">
+            <div className="item-4 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">Kickboxing</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Sunday: 6:00pm-7:00pm
               </p>
             </div>
-          </div>
+          </Link>
 
-          <div className="item-5-div relative">
-            <div className="item-5 ease-in duration-[0.4s] p-6"></div>
+          <Link href={'/schedule/monday'} className="item-5-div relative">
+            <div className="item-5 ease-in duration-[0.4s]"></div>
             <div className="absolute z-10 bottom-10 left-10">
-              <p className="text-white text-[3rem] font-bold">Strength & Conditioning</p>
-              <p className="text-white bg-[#FF0336] text-[16px] mt-3 py-1 px-5">
+              <p className="text-white text-[3rem] font-bold">
+                Strength & Conditioning
+              </p>
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
                 Monday: 4:00pm-5:00pm
               </p>
             </div>
-          </div>
+          </Link>
         </div>
       </div>
     </section>
