659e5c55c8cd6e73097bd88561dbea36d968d8fa
Edward Park <edwardpark123@gmail.com>
2023-09-04 15:47:57 -0700

    relocated gallery images, Gallery component

--- app/components/gallery/gallery.tsx
+++ app/components/gallery/gallery.tsx
@@ -1,21 +1,37 @@
-import React from 'react';
+import Image from 'next/image';
+import TitleBg from '../../images/title-bg.svg';
 
 export default function Gallery() {
   return (
     <section>
-      <div className="container page-padding py-[4rem]">
-        <div className="flex flex-col text-center relative items-center ">
-          <p className="text-black relative z-10 text-[15px] uppercase font-bold mb-10">
-            Gallery
-          </p>
-        </div>
-        <div className="gallery-grid py-24 w-full h-auto relative ">
-          <div id="item-0"></div>
-          <div id="item-1"></div>
-          <div id="item-2"></div>
-          <div id="item-3"></div>
-          <div id="item-4"></div>
-          <div id="item-5"></div>
+      <div
+        className="container page-padding flex flex-col items-center relative gap-[0rem] maxXl:flex-wrap py-[12rem]"
+        style={{ margin: '0 auto' }}
+      >
+        <p className="text-white relative z-10 text-[16px] uppercase font-bold mb-10">
+          gallery
+        </p>
+        <Image
+          src={TitleBg}
+          alt="text_bg"
+          className="color-logo w-[21rem] absolute -top-[-8px]"
+        />
+        <h2 className="text-[34px] text-black font-bold mb-4 ">
+          Moments Captured At Colosseum Combat
+        </h2>
+
+        <div className="grid grid-rows-3 grid-cols-5 py-24 gap-[6px] w-full h-auto maxMd:flex maxMd:flex-col">
+          <div id="item-0" className="maxMd:h-[41rem] maxMd:w-full"></div>
+
+          <div id="item-1" className="maxMd:h-[41rem] maxMd:w-full"></div>
+
+          <div id="item-2" className="maxMd:h-[41rem] maxMd:w-full"></div>
+
+          <div id="item-3" className="maxMd:hidden"></div>
+
+          <div id="item-4" className="maxMd:hidden"></div>
+
+          <div id="item-5" className="maxMd:hidden"></div>
         </div>
       </div>
     </section>
