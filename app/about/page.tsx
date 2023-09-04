b9867bad622fe2a406eccd7e28964d8ca9eac0ad
Edward Park <edwardpark123@gmail.com>
2023-09-04 15:46:54 -0700

    About page and component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,16 @@
-export default function About() {
+import About from '../components/about/about';
+import Coaches from '../components/coaches/coaches';
+
+export default function AboutPage() {
   return (
-    <section className='bg-[#ffb703]'>
-      <div>
-about
+    <section className="">
+      <div className="schedule-banner relative justify-center flex">
+        <h1 className="text-white absolute bottom-[25px] text-[3rem] font-bold">
+          About
+        </h1>
       </div>
+      <About />
+      <Coaches />
     </section>
   );
 }
--- app/components/about/about.tsx
+++ app/components/about/about.tsx
@@ -1,39 +1,33 @@
 import Image from 'next/image';
-import AboutImage from './about.jpeg';
+import AboutImage from '../../images/about/about.jpeg';
 
 const About: React.FC = () => {
+
   return (
-    <section className="bg-[#ffb703] py-16 md:py-24 relative">
-      <div className="container mx-auto flex md:flex-row items-center">
-        <div className="text-white md:w-1/2 relative">
-          <div className="about-text bg-black px-12 py-8 w-2/3 ">
-            <h2 className="text-4xl font-semibold mb-4 md:mb-8">
-              About FF Academy
-            </h2>
-            <p className="text-lg mb-6">
-              Since 2011, we have been providing high-quality, kickboxing
-              instruction that showcases how effective kickboxing is as a
-              martial art and as a killer workout. Whether you&apos;re looking
-              to shed a few pounds, pick up a fun new hobby, or just get in the
-              best shape of your life, you&apos;ll find what you&apos;re looking
-              for at FF Academy.
-            </p>
-            <p className="text-lg">
-              Conveniently located in Koreatown, Los Angeles, we offer a 7,000
-              square foot open gym, top of the line equipment, and a safe,
-              learning environment where you can learn kickboxing with the help
-              of knowledgeable instructors and helpful, like-minded training
-              partners.
-            </p>
-          </div>
+    <section className="about-section">
+      <div className="container page-padding  flex flex-row maxLg:flex-col maxLg:items-center  py-[12.5rem] gap-3">
+        <div className="bg-black flex flex-col justify-evenly text-white px-12 py-8 w-1/2 maxLg:w-full">
+          <h2 className="text-5xl maxXl:text-3xl font-semibold mb-4 uppercase text-left">
+            About Colosseum Combat
+          </h2>
+          <p className="text-3xl mb-6  maxLg:text-xl">
+            Since 2011, we have been providing high-quality, kickboxing
+            instruction that showcases how effective kickboxing is as a martial
+            art and as a killer workout. Whether you&apos;re looking to shed a
+            few pounds, pick up a fun new hobby, or just get in the best shape
+            of your life, you&apos;ll find what you&apos;re looking for at FF
+            Academy.
+          </p>
+          <p className="text-3xl maxLg:text-xl">
+            Conveniently located in Los Angeles, we offer a 7,000 square foot
+            open gym, top of the line equipment, and a safe, learning
+            environment where you can learn kickboxing with the help of
+            knowledgeable instructors and helpful, like-minded training
+            partners.
+          </p>
         </div>
-        <div className="md:w-1/2">
-          <Image
-            src={AboutImage}
-            alt="About Us"
-            className="w-full h-800  md:mb-0 mb-8"
-          />
-        </div>
+
+        <Image src={AboutImage} alt="About Us" className="w-1/2 maxLg:w-full" />
       </div>
     </section>
   );
