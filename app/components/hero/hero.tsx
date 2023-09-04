4a3b6d9f443abb846161df90a8362199a7b13fc4
Edward Park <edwardpark123@gmail.com>
2023-09-04 15:52:10 -0700

    Hero component, about image

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -1,21 +1,22 @@
-import Link from 'next/link';
+import Button from '../button/button';
 
 export default function Hero() {
   return (
     <section className="hero-section min-h-screen bg-gray-800 flex items-center">
       <div className="container mx-auto px-4">
-        <div className="text-right uppercase">
-          <h1 className="text-3xl sm:text-5xl md:text-6xl text-white font-bold mb-4">
+        <div className="text-right maxSm:text-center uppercase">
+          <h1 className="maxSm:text-3xl maxMd:text-5xl text-7xl text-white font-bold mb-4">
             Unleash Your Inner Warrior
           </h1>
-          <p className="text-lg sm:text-xl text-gray-300 mb-8">
-            Train Hard, Fight Strong at Fusion Fight Academy!
+          <p className="maxSm:text-lg text-4xl text-gray-300 mb-8">
+            Train Hard, Fight Strong at Colosseum Combat
           </p>
-          <Link href="/classes">
-            <button className="bg-blue-500 text-white px-6 py-3 rounded-lg font-semibold hover:bg-blue-600">
-              Our Classes
-            </button>
-          </Link>
+
+          <Button
+            text="Learn More"
+            goTo="/about"
+            className="text-[15px] bg-[#ffb703] text-center px-12 py-4 font-[600] w-[20rem] h-[5.5rem] uppercase hero-cta ml-2 hover:text-white"
+          />
         </div>
       </div>
     </section>
