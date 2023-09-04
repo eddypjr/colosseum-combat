2af3f5d7dfe520fdf3f9e7c378144c94660710f2
Edward Park <edwardpark123@gmail.com>
2023-09-04 15:46:13 -0700

    Coaches component

--- app/components/coaches/coaches.tsx
+++ app/components/coaches/coaches.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { useState } from 'react';
 import Image from 'next/image';
-import EddieBravo from './eddie-bravo.png';
-import WonderBoy from './wonderboy.png';
-import Floyd from './floyd.png';
+import EddieBravo from '../../images/coaches/eddie-bravo.png';
+import WonderBoy from '../../images/coaches/wonderboy.png';
+import Floyd from '../../images/coaches/floyd.png';
+import TitleBg from '../../images/title-bg.svg';
 
 export default function Coaches() {
   const [imgHover, setImgHover] = useState(false);
@@ -22,44 +23,56 @@ export default function Coaches() {
     { img: Floyd, name: 'Floyd Mayweather', job: 'Boxing Coach' },
   ];
   return (
-    <>
+    <section className="classes-section">
       <div
-        className="flex justify-center gap-[5rem] md1200:flex-wrap bg-[#ffb703]"
+        className="container page-padding flex flex-col items-center relative gap-[0rem] maxXl:flex-wrap py-[12rem]"
         style={{ margin: '0 auto' }}
       >
-        {trainers.map((train, id) => (
-          <div
-            onMouseEnter={hoverBox}
-            onMouseLeave={hoverBoxRemove}
-            key={id}
-            className="relative cursor-pointer w-[35rem] min450:w-full flex flex-col select-none"
-          >
-            {/* trainer img */}
-            <Image
-              src={train.img}
-              alt="trainer"
-              style={{ transition: 'all 0.3s' }}
-              className={`w-[22rem] z-10 relative grayscale mx-auto ${
-                imgHover ? 'hover:grayscale-0' : ''
-              }`}
-            />
-            {/* trainer description */}
-            <div className="box-desc bg-white w-full text-center shadow-lg z-20 rounded-[6px] px-[20px] py-[30px] absolute -bottom-[31px]">
-              <i className="fa-solid fa-angle-up absolute -top-[16px] left-[162px] text-3xl"></i>
-              <h3 className="font-bold text-[2.4rem] ">{train.name}</h3>
-              <p className="font-medium text-[1.5rem] text-[#646464]">
-                {train.job}
-              </p>
-              <div className="flex gap-8 text-[#646464] w-full justify-center mt-5 text-[1.6rem]">
-                <i className="fa-brands fa-facebook-f"></i>
-                <i className="fa-brands fa-twitter"></i>
-                <i className="fa-brands fa-instagram"></i>
-                <i className="fa-regular fa-envelope"></i>
+        <p className="text-white relative z-10 text-[16px] uppercase font-bold mb-10">
+          Coaches
+        </p>
+        <Image
+          src={TitleBg}
+          alt="stroke"
+          className="color-logo w-[21rem] absolute -top-[-10px]"
+        />
+        <h2 className="text-[34px] text-black font-bold mb-4 ">
+          Meet Our World Class Coaches
+        </h2>
+        <p className="text-[#646464] font-medium text-[15px] ">
+          Team up with our remarkable coaching experts to reach new heights.
+          Benefit
+          <br />
+          from personalized guidance and an endless reservoir of motivation!
+        </p>
+        <div className="flex gap-3 maxLg:gap-y-16 mt-20 justify-center maxLg:items-center maxLg:flex-col w-full">
+          {trainers.map((train, id) => (
+            <div
+              onMouseEnter={hoverBox}
+              onMouseLeave={hoverBoxRemove}
+              key={id}
+              className="relative cursor-pointer w-[35rem] maxXs:w-full flex flex-col select-none h-[350px]"
+            >
+              <Image
+                src={train.img}
+                alt="trainer"
+                style={{ transition: 'all 0.3s' }}
+                className={`w-[22rem] z-10 absolute grayscale mx-auto flex place-self-center h-3/4 py-5 ${
+                  imgHover ? 'hover:grayscale-0' : ''
+                }`}
+              />
+              <div className="image-background absolute top-[4px] h-full w-full scale-[1]" />
+              <div className="box-desc bg-white w-full text-center shadow-lg z-20 rounded-[6px] px-[20px] py-[30px] absolute -bottom-[31px]">
+                <h3 className="font-bold text-[2.4rem] ">{train.name}</h3>
+                <p className="font-medium text-[1.5rem] text-[#646464]">
+                  {train.job}
+                </p>
+                <div className="flex gap-8 text-[#646464] w-full justify-center mt-5 text-[1.6rem]"></div>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </>
+    </section>
   );
 }
