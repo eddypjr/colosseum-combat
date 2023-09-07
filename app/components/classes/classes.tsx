16af88307e11e8a09e582e5679d59d56e70c3b68
Edward Park <edwardpark123@gmail.com>
2023-09-06 17:04:10 -0700

    added missing width and heights to Next Image

--- app/components/classes/classes.tsx
+++ app/components/classes/classes.tsx
@@ -1,11 +1,5 @@
 import Image from 'next/image';
 import TitleBg from '../../images/title-bg.svg';
-import BJJ from '../../images/classes/bjj.jpg';
-import Boxing from '../../images/classes/boxing.jpg';
-import KB from '../../images/classes/kb.jpg';
-import MMA from '../../images/classes/mma.jpg';
-import NoGi from '../../images/classes/no-gi.jpg';
-import Strength from '../../images/classes/strength.jpg';
 import Link from 'next/link';
 
 export default function Classes() {
@@ -30,7 +24,9 @@ export default function Classes() {
         <div className="grid grid-cols-4 grid-rows-2 gap-5 h-full mt-7 maxMd:flex maxMd:flex-col">
           <Link href={'/schedule/monday'} className="item-0-div relative">
             <Image
-              src={BJJ}
+              width={706.5}
+              height={360}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887126/bjj_sd6kbv.webp"
               alt="bjj"
               blurDataURL="URL"
               placeholder="blur"
@@ -40,7 +36,7 @@ export default function Classes() {
               <p className="text-white text-[3rem] font-bold">
                 Brazilian Jiu Jitsu
               </p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Wednesday: 9:00am-10:00am
               </p>
             </div>
@@ -48,7 +44,9 @@ export default function Classes() {
 
           <Link href={'/schedule/monday'} className="item-1-div relative">
             <Image
-              src={MMA}
+              width={345.75}
+              height={360}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887135/mma_socgrg.webp"
               alt="MMA"
               blurDataURL="URL"
               placeholder="blur"
@@ -56,7 +54,7 @@ export default function Classes() {
             />
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">MMA</p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Friday: 10:00am-11:00am
               </p>
             </div>
@@ -64,7 +62,9 @@ export default function Classes() {
 
           <Link href={'/schedule/monday'} className="item-2-div relative">
             <Image
-              src={Boxing}
+              width={345.75}
+              height={360}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887129/boxing_sau01t.webp"
               alt="Boxing"
               blurDataURL="URL"
               placeholder="blur"
@@ -72,7 +72,7 @@ export default function Classes() {
             />
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">Boxing</p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Saturday: 9:00am-10:00am
               </p>
             </div>
@@ -80,7 +80,9 @@ export default function Classes() {
 
           <Link href={'/schedule/monday'} className="item-3-div relative">
             <Image
-              src={NoGi}
+              width={345.75}
+              height={0}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887133/no-gi_glhri6.webp"
               alt="No Gi Grappling"
               blurDataURL="URL"
               placeholder="blur"
@@ -90,7 +92,7 @@ export default function Classes() {
               <p className="text-white text-[3rem] font-bold">
                 No Gi Grappling
               </p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Friday: 1:00pm-2:00pm
               </p>
             </div>
@@ -98,7 +100,9 @@ export default function Classes() {
 
           <Link href={'/schedule/monday'} className="item-4-div relative">
             <Image
-              src={KB}
+              width={345.75}
+              height={360}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887130/kb_i54yx9.webp"
               alt="Kickboxing"
               blurDataURL="URL"
               placeholder="blur"
@@ -106,7 +110,7 @@ export default function Classes() {
             />
             <div className="absolute z-10 bottom-10 left-10">
               <p className="text-white text-[3rem] font-bold">Kickboxing</p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Sunday: 6:00pm-7:00pm
               </p>
             </div>
@@ -114,7 +118,9 @@ export default function Classes() {
 
           <Link href={'/schedule/monday'} className="item-5-div relative">
             <Image
-              src={Strength}
+              width={706.5}
+              height={360}
+              src="https://res.cloudinary.com/dxzi6ahcb/image/upload/f_auto,q_auto/v1693887124/strength_ibqcoz.webp"
               alt="Strength & Conditioning"
               blurDataURL="URL"
               placeholder="blur"
@@ -124,7 +130,7 @@ export default function Classes() {
               <p className="text-white text-[3rem] font-bold">
                 Strength & Conditioning
               </p>
-              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5">
+              <p className="text-black bg-[#ffb703] text-[16px] mt-3 py-1 px-5 maxLg:w-fit">
                 Monday: 4:00pm-5:00pm
               </p>
             </div>
