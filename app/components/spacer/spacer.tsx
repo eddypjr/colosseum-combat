299cd028f95d01a2c1017d6b0647615b2853199c
Edward Park <edwardpark123@gmail.com>
2023-09-06 17:27:11 -0700

    created temporary spacer component

---
+++ app/components/spacer/spacer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Spacer() {
+  return (
+    <>
+      <div className="h-[1px] w-full flex items-center justify-center">
+        <div className="text-black bg-[#64646438] h-1 w-[95%]" />
+      </div>
+    </>
+  );
+}
