6939563e53fef3616c904373565c524ad3c328e9
Edward Park <edwardpark123@gmail.com>
2023-09-06 17:04:29 -0700

    api route for schedule

---
+++ app/api/schedule/[day]/route.ts
@@ -0,0 +1,16 @@
+import { Params } from '@/app/schedule/[day]/page';
+import connectMongoDb from '@/app/utils/mongodb';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(request: NextRequest, { params: { day } }: Params) {
+  const client = await connectMongoDb;
+  const db = client.db('colosseum-combat');
+  const schedulesCollection = db.collection('schedule');
+
+  try {
+    const schedule = await schedulesCollection.find({ day }).toArray();
+    return NextResponse.json(schedule);
+  } catch (error) {
+    return console.log('Error fetching schedules', 500);
+  }
+}
